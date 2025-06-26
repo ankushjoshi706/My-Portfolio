7cbf1cb93a04e19b85e5706e4a3a8e80909c22b5
ankushjoshi706 <85740434+ankushjoshi706@users.noreply.github.com>
2025-06-26 18:49:51 +0530

    first commit

---
+++ src/componets/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I am Ankush Joshi</h1>
+            <p className={styles.description}>
+                I'm a Aspiring Frontend Developer | Proficient in HTML, CSS, JavaScript, React | Passionate about Building Responsive Web Interfaces
+            </p>
+            <a href="mailto:ankushjoshi706@gmail.com" className={styles.contentBtn}>Contact Me</a>
+        </div>
+        <img src="./assets/hero/smile-boy.jpg" alt="Hero image of Me" className={styles.heroImg}></img>
+
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section>
+  )
+}
