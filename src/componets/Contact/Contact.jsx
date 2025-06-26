76afa63089c01481c2399be5e3411dd53348084c
ankushjoshi706 <85740434+ankushjoshi706@users.noreply.github.com>
2025-06-26 18:37:40 +0530

    first commit

---
+++ src/componets/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+  return (
+    <footer className={styles.container} id='contact'>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Fell free to react out!</p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src="./assets/contact/emailIcon.png" alt="Email Icon" />
+                <a href="mailto:ankushjoshi706@hmail.com">Myemail@email.com</a>
+            </li>
+
+            <li className={styles.link}>
+                <img src="./assets/contact/linkedinIcon.png" alt="Linkedin Icon" />
+                <a href="https://www.linkedin.com/in/ankush-joshi-56a94418b ">Linkedin.com</a>
+            </li>
+
+            <li className={styles.link}>
+                <img src="./assets/contact/githubIcon.png" alt="Github Icon" />
+                <a href="https://github.com/ankushjoshi706">Github.com</a>
+            </li>
+            
+        </ul>
+    </footer>
+  )
+}
